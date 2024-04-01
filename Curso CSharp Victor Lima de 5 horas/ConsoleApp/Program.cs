17c61742463a3e234759fcd75192a182f291409d
Carlos Alberto <carlosalbertoar@hotmail.com>
2024-03-31 22:25:37 -0300

    Curso C# em 5 horas (2:11 Condicionais Teoria)

---
+++ Curso CSharp Victor Lima de 5 horas/ConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Console.WriteLine("Hello, World!");
+            */
+
+            /* Variaveis
+            int segundaGerraMundial = 1942;
+            string corFavorita = "Azul";
+            float velocidadeF1 = 294.48f;
+            bool ocorreuSegundaGerraMundial = true;
+            
+            Console.WriteLine(segundaGerraMundial);
+            Console.WriteLine(corFavorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(ocorreuSegundaGerraMundial);
+
+            velocidadeF1 = 300f;
+            corFavorita = "Roxo" ;
+
+            Console.WriteLine(corFavorita);
+            Console.WriteLine(velocidadeF1);
+
+            */
+
+            /* Capturando entrada do usuário
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Bem vindo " + nome);
+            
+            */
+
+            /* Operações matemáticas */
+
+            int numeroQualquer = 20 + 20 - 5;
+            int numeroMultiplicado = 10 * 90;
+            float numeroDividido = 90 / 10;
+            Console.WriteLine(numeroQualquer);
+            Console.WriteLine(numeroMultiplicado);
+            Console.WriteLine(numeroDividido);
+
+
+            
+            
+
+
+        }
+    }    
+}
+
